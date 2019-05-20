0a37ed4919e0bf8fed3b4e24553d775a649d8e6d
415811958 <415811958@qq.com>
2019-05-20 19:13:42 +0800

    添加实体类

---
+++ PullDemo/src/com/wgwzhao/utils/Person.java
@@ -0,0 +1,46 @@
+package com.wgwzhao.utils;
+
+import java.io.Serializable;
+
+/**
+ * @author wgw
+ * @date 2019/5/20 19:09
+ */
+public class Person implements Serializable {
+
+    
+
+    public Person(String name, int age, int score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+    private String name;
+    private int age;
+    private int score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
